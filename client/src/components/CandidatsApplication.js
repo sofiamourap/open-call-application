c75091fb582d62183e0f7eee562f2e0500f78e8d
Sofia Moura <sofiamourap@gmail.com>
2021-02-19 18:51:32 +0000

    upload project without axios

--- client/src/components/CandidatsApplication.js
+++ client/src/components/CandidatsApplication.js
@@ -5,6 +5,31 @@ import Gallery from "./Gallery";
 export default function CandidatsApplication() {
   const [openCallInfo, setOpenCallInfo] = useState([]);
   const { id } = useParams();
+  const [newProject, setNewProject] = useState({
+    full_name: "",
+    email: "",
+    project: "",
+  });
+
+  //I need to fetch /opencall/:id/candidats to post a new project
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    postNewProject();
+  };
+
+  const postNewProject = async () => {
+    try {
+      const response = await fetch(`/opencall/${id}/candidats`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newProject),
+      }).then((res) => console.log(res));
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const getOpenCallInfo = () => {
     fetch(`/opencall/${id}`)
@@ -17,17 +42,60 @@ export default function CandidatsApplication() {
   useEffect(() => {
     getOpenCallInfo();
   }, []);
+
+  const handleChange = ({ target }) => {
+    setNewProject((state) => ({ ...state, [target.name]: target.value }));
+  };
+  // function handleChange(event) {
+  //   const { value } = event.target;
+  //   setInput({
+  //     ...input,
+  //     [event.target.name]: value,
+  //   });
+  // }
+
   return (
     <div>
       {openCallInfo.map((inf) => (
         <div key={inf.id}>
-          {console.log(inf)}
+          {/* {console.log(inf)} */}
           <h1>{inf.residency_name}</h1>
           <h2>{inf.name}</h2>
 
           <li>{inf.description}</li>
         </div>
       ))}
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Full name{" "}
+            <input
+              type="text"
+              name="full_name"
+              value={newProject.full_name}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Email{" "}
+            <input
+              name="email"
+              value={newProject.email}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Project{" "}
+            <input
+              name="project"
+              value={newProject.project}
+              onChange={handleChange}
+            />
+          </label>
+
+          <button className="btn btn-outline-success">Submit</button>
+        </form>
+      </div>
       <Switch>
         <Route path="/gallery/:id">
           <Gallery />
