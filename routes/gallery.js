2a65895fd3f0bacd0b34e330e0eca4fef7045c4e
Sofia Moura <sofiamourap@gmail.com>
2021-02-23 10:00:54 +0000

    fixing typo in gallery fetch BE

--- routes/gallery.js
+++ routes/gallery.js
@@ -26,7 +26,6 @@ router.get("/:id/opencalls", function (req, res) {
 //GET Gallery by id
 router.get("/:id", function (req, res) {
   const { id } = req.params;
-  c;
   db(`SELECT * FROM gallery WHERE id="${id}";`)
     .then((result) => {
       res.send(result.data);
